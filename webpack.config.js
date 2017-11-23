c3f6e991f62adf2c63ae80f7ef78ab869fe9798d
temilolu <temitayo094@gmail.com>
2017-11-23 15:19:34 +0100

    Recent updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,64 +2,58 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-  entry: './src/index.jsx',
-  output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'public'),
-      publicPath: 'http://localhost:8080/'
-  },
+	entry: './src/index.jsx',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'public'),
+		publicPath: 'http://localhost:8080/',
+	},
 
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './public/',
-    historyApiFallback: true
-  },
-  resolve:{
-    extensions: ['.js', '.jsx', '.json', '.scss', '.css']
-  },
-  plugins: [
-      
-  ],
+	devtool: 'source-map',
+	devServer: {
+		contentBase: './public/',
+		historyApiFallback: true,
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
+	},
+	plugins: [],
 
-  module:{ 
-      
-      rules: [
-        // JSX
-        {
-            test:/\.jsx$/,
-            loaders:['babel-loader'],
-            exclude: /node_modules/    
-        },
+	module: {
+		rules: [
+			// JSX
+			{
+				test: /\.jsx$/,
+				loaders: ['babel-loader'],
+				exclude: /node_modules/,
+			},
 
-        // SASS
-        {
-            test:/\.scss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader",
-                options: {
-                    includePaths: ["absolute/path/a", "absolute/path/b"]
-                }
-            }]
-        },
+			// SASS
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							includePaths: ['absolute/path/a', 'absolute/path/b'],
+						},
+					},
+				],
+			},
 
-        // Images
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-            'file-loader'
-            ]
-        }
-
-
-
-
-      ]
-  }
-}
-
+			// Images
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ['file-loader'],
+			},
+		],
+	},
+};
 
 module.exports = config;
