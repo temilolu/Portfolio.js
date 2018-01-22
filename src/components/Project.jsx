b7bd7403d7b1595834379e1172e17f0d5faab86a
temitayo ogunlolu <temitayo094@gmail.com>
2018-01-22 23:52:52 +0100

    Server.js for deploy

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,34 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Project extends Component{
-
-    constructor(props){
+class Project extends Component {
+    // 
+    constructor(props) {
         super(props)
 
         this.state = { repos: [] }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         fetch('https://api.github.com/users/temilolu/repos?sort=pushed')
-        .then(response => response.json())
-        .then(repos => this.setState({ repos }))
+            .then(response => response.json())
+            .then(repos => this.setState({ repos }))
     }
-    render(){
+    render() {
 
-         // If the state doesn't have a user key, it means the AJAX didn't complete yet. Simply render a LOADING indicator.
-         if (!this.state.repos) {
+        // If the state doesn't have a user key, it means the AJAX didn't complete yet. Simply render a LOADING indicator.
+        if (!this.state.repos) {
             return (<div className="user-page">LOADING...</div>);
-      }
-        return(
+        }
+        return (
             <ul>
                 {this.state.repos.map(repo => (
                     <li key={repo.id}>
-                    
+
                         <div className="col-md-4">
-                        <Link to={repo.html_url} target="_blank">
+                            <Link to={repo.html_url} target="_blank">
                                 <div className="project--box">
-                                    
+
                                     <div className="project--box--title">
                                         <h3>{repo.name}</h3>
                                         <ul className="list-inline">
@@ -37,22 +37,22 @@ class Project extends Component{
                                         </ul>
                                         <p>{repo.description}</p>
                                         <span>{repo.language}</span>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
                         </div>
-                  
-                </li>  
+
+                    </li>
                 ))}
-              
-             </ul>
-            
+
+            </ul>
+
         );
     }
 }
 
 Project.propTypes = {
-   name: React.PropTypes.string,
-   description: React.PropTypes.string
+    name: React.PropTypes.string,
+    description: React.PropTypes.string
 }
 export default Project;
